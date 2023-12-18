0a5df7d46091781edc85881ccc20b697f5a587ea
lucas <lucas.chorolqui@gmail.com>
2023-12-17 23:51:26 -0300

    updates

--- src/components/Movement.jsx
+++ src/components/Movement.jsx
@@ -6,29 +6,7 @@ function Movement({ movement }) {
   const { completeDate } = useAccount();
   return (
     <div className="w-full h-[4rem] bg-[#f2f2f2] rounded-md py-2 px-6 flex items-center justify-between max-sm:flex-col max-sm:h-[5rem]">
-      <div className="flex max-sm:text-sm">
-        <FontAwesomeIcon icon={faMoneyBillTransfer} color="green" size="xl" />
-        <p className="text-gray-900 font-semibold ml-2 first-letter:uppercase ">
-          {movement.type}
-        </p>
-      </div>
-
-      <div className="text-gray-900 flex flex-col items-center">
-        <p
-          className={`font-bold max-sm:text-sm
-          ${movement.type === "deposit" ? "text-green-600" : ""} 
-          ${movement.type === "loan" ? "text-green-600" : ""}`}
-        >
-          {movement.type === "deposit" ? "+" : ""}
-          {movement.type === "withdraw" ? "-" : ""}
-          {movement.type === "payments" ? "-" : ""}
-          {movement.type === "loan" ? "+" : ""}
-          {movement.type === "transfer" ? "-" : ""} $ {movement.balance} USD
-        </p>
-
-        <p className="text-gray-900 text-xs max-sm:font-extralight font-light">
-          {completeDate}
-        </p>
+     
       </div>
     </div>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -64,6 +64,47 @@ function Login() {
                 with <strong className="text-green-600">Oslo</strong>!
               </p>
             </article>
+
+            <form
+              className="relative col-start-3 flex h-full w-full items-center justify-center max-lg:col-start-2 max-lg:col-end-6 max-md:col-start-1 max-md:col-end-7"
+              onSubmit={handleSubmit}
+            >
+              <div
+                className="relative flex h-full w-[22rem]
+             flex-col justify-evenly rounded-[3rem] bg-green-600 p-6 shadow-2xl"
+              >
+                <p className="relative text-3xl font-bold text-white">
+                  Email address
+                  <span className="absolute left-[0] top-[2.2rem] h-[3px] w-[8rem] bg-gray-900"></span>
+                </p>
+                <input
+                  type="email"
+                  className={inputStyles}
+                  placeholder="admin@admin.com"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
+                <p className="relative text-3xl font-bold text-white">
+                  Password
+                  <span className="absolute left-[0] top-[2.2rem] h-[3px] w-[5rem] bg-gray-900"></span>
+                </p>
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  className={inputStyles}
+                  placeholder="admin"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                />
+
+                <div className="flex justify-end">
+                  <button className=" h-[3rem] w-full rounded-2xl bg-gray-900 p-3 font-bold text-white transition-all duration-300 ease-in hover:bg-gray-950 hover:shadow-2xl">
+                    Login
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </section>
