577899c743b7f5987457dace34dc03a16caf8adb
lucas <lucas.chorolqui@gmail.com>
2024-01-04 23:55:26 -0300

    updates

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 function ProtectedRoute({ children }) {
   const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
+
   useEffect(() => {
     if (!isAuthenticated) navigate("/");
   }, [isAuthenticated, navigate]);
